88de70df218289a1b49ab499259d0eb5cb61c97d
Alec McFarlin <71944837+ajmcfarlin@users.noreply.github.com>
2022-04-12 09:06:17 -0500

    Update dbConnect.php

--- src/dbConnect.php
+++ src/dbConnect.php
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "dbtest", "pwd" => "Lexar-1395", "Database" => "productsDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:wdv495db.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+// $connectionInfo = array("UID" => "dbtest", "pwd" => "Lexar-1395", "Database" => "productsDB", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+// $serverName = "tcp:wdv495db.database.windows.net,1433";
+// $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
